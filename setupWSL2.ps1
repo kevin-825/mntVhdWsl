35fe7ff1e1fd337654a0a31e33bc82d9e4c70593
Kevin Aimaier <kflyn825@outlook.com>
2026-09-06 16:51:38 +0800

    feat: now this script will scan all distro and all users of each distro and sync the generated .bash_alias_1 file into each of them

--- setupWSL2.ps1
+++ setupWSL2.ps1
@@ -83,12 +83,42 @@ if (Test-Path $procRuntime) {
     if ($LASTEXITCODE -ne 0) { throw "process-runtime.ps1 failed." }
 }
 
-# --- Execution ---
+# --- Sync  geenarted .bash_aliases_1 files into each user of each distro---
 
 $localFile  = ".\.bash_aliases_1"
-$targetFile = "\\wsl$\Ubuntu-24.04\home\kflyn\.bash_aliases_1"
 
-Sync-FileIfChanged_contentbased -SourcePath $localFile -TargetPath $targetFile
+# Get all installed distros
+$distros = wsl.exe -l -q |
+    ForEach-Object { $_.Trim() } |
+    Where-Object { $_ -ne "" } |
+    ForEach-Object { $_ -replace '[^\x20-\x7E]', '' }
+
+Write-Host "Found distros: $($distros -join ', ')"
+foreach ($distro in $distros) {
+
+    Write-Host "===$distro==="
+
+    # Get Linux users (UID >= 1000)
+    $passwd = wsl.exe -d $distro -- cat /etc/passwd
+    #Write-Host "Contents of /etc/passwd: $($passwd -join "`n")"
+    $users = $passwd |
+    ForEach-Object { $_.Trim() } |
+    Select-String "^[^:]+:[^:]*:[1-9][0-9]{3}:" |
+    ForEach-Object { ($_ -split ":")[0] }
+
+    
+    Write-Host "Found users: $($users -join ', ')"
+
+    foreach ($user in $users) {
+        Write-Host "  -> Syncing for user: $user"
+
+        $targetFile = "\\wsl$\$distro\home\$user\.bash_aliases_1"
+
+        Sync-FileIfChanged_contentbased `
+            -SourcePath $localFile `
+            -TargetPath $targetFile
+    }
+}
 
 Write-Host "`n=== setup.ps1 completed successfully ==="
-#wsl.exe --cd ~
+pause "Press any key to exit..."
