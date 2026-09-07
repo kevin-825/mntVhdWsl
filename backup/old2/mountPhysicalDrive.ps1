2407202d468eb05f079c0dd6c04b5c6fee9d50de
Kevin Aimaier <kflyn825@outlook.com>
2026-09-07 17:50:45 +0800

    delete old files

--- backup/old2/mountPhysicalDrive.ps1
+++
@@ -1,6 +0,0 @@
-wsl --mount \\.\PHYSICALDRIVE1 --bare
-wsl --mount \\.\PHYSICALDRIVE2 --bare
-#wsl --mount \\.\PHYSICALDRIVE5 --bare
-
-
-
--- backup/old3/Readme.txt
+++
@@ -1,6 +0,0 @@
-put the following file  under home/$USER/ dir of wsl2 distro:
-.bash_aliases  
-wsl-mount-ramdisk.sh
-
-
-
